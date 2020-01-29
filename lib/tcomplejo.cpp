264b93c2dd00761eb992d3156a42ec60444b5cdb
app126 <32137021+app126@users.noreply.github.com>
2020-01-29 20:47:38 +0100

    Anyadido lo que he hecho en clase.
    Anyadido lo que he hecho en clase.

--- lib/tcomplejo.cpp
+++ lib/tcomplejo.cpp
@@ -1,1 +1,44 @@
-#include <iostream>
+#include <iostream> 
+using namespace std;
+ 
+class TComplejo {
+   private:// Datos privados de la clase tcomplejo
+      double re, im; 
+
+    //Constructor por defecto.
+      void TComplejo() {
+      }
+
+    //Constructor a partir de la parte real.
+      void TComplejo (double){
+      }
+
+    //Constructor a partir de la parte real e imaginaria
+      void TComplejo (double, double){
+      }
+
+    //Constructor copia
+      void TComplejo(Tcomplejo &){
+      }
+      
+    //Destructor      
+      ~TComplejo();
+
+//////////////////////METODOS///////////////////////////////////
+
+
+
+};
+
+
+/*int main() {
+   pareja par1;
+   int x, y;
+   
+   par1.Guarda(12, 32);
+   par1.Lee(x, y);
+   cout << "Valor de par1.a: " << x << endl;
+   cout << "Valor de par1.b: " << y << endl;
+
+   return 0;
+}*/
